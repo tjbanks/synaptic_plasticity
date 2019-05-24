c351c11108beec7190c9b3c4bd0d185634dfde77
Tyler Banks <tbg28@mizzou.edu>
2019-05-24 11:01:51 -0500

    working example

--- bmtk_model/gen_input.py
+++ bmtk_model/gen_input.py
@@ -1,6 +1,6 @@
 from bmtk.utils.spike_trains import SpikesGenerator
 
-def write_input_file(rate=20):
+def write_input_file(rate=50):
     sg = SpikesGenerator(nodes='network/input_nodes.h5', t_max=1.0)
     sg.set_rate(rate)
     sg.save_csv('./input/input_spikes.csv', in_ms=True)
--- bmtk_model/plot_test.py
+++ bmtk_model/plot_test.py
@@ -1,3 +1,16 @@
 from bmtk.analyzer.cell_vars import plot_report
 
-plot_report(config_file='simulation_config.json')
+#plot_report(config_file='simulation_config.json')
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = h5py.File('output/syn_report.h5')
+data = [l[0] for l in list(f['data'])]
+data = np.abs(data)
+
+plt.plot(data)
+plt.ylabel('synaptic current')
+plt.show()
+ 
