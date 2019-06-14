1be79626f5533debe3d9e0a26073d7f319da0a65
Tyler Banks <tbg28@mail.missouri.edu>
2019-06-14 15:29:33 -0500

    work

--- neurosim.py
+++ neurosim.py
@@ -1,10 +1,12 @@
+
 # Written by Tyler Banks
 
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-from scipy.integrate import odeint
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html
+from scipy.integrate import odeint,solve_ivp
 from scipy.interpolate import interp1d
 
 def alpha_m(v):
@@ -45,7 +47,7 @@ def n_tau(v=0.0):
     #return 14.4 - 12.8/(1.0+(np.exp((v+28.3)/(-19.2))))
 
 def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
-            gNa = 120, gK = 36, gL = 0.3, v_init = -70,
+            gNa = 120, gK = 36, gL = 0.3, v_init = -70, gsyn=.20,
             inj_amp = 10, inj_start = 50, inj_dur = 20,
             syn_rev=0, syn_tau=0.1, syn_weight=0.4, syn_times = [50.0],
             f=1, d1=1, d2=1, tauF=1,tauD1=1,tauD2=1,
@@ -53,17 +55,19 @@ def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
 
     def I(t,amp,start,dur):
         return np.piecewise(t, [t<start, t>=start, t>=start+dur], [0,amp,0])
-   
-    tsyn = -1000
-    F = 0
-    D1 = 1
-    D2 = 1
-    facfactor = 1
+    
+    #tsyn = -10000
+    #F = 0
+    #D1 = 1
+    #D2 = 1
+    #facfactor = 1
+    #on_syn = 0
+    
 
-    def hh_eq(y,t,inj_amp,inj_start,inj_dur,syn_times):
-        dy = np.zeros((10,))
-       
-        nonlocal tsyn,F,D1,D2,facfactor
+    def hh_eq(t,y):
+        dy = np.zeros((17,))
+        
+        #nonlocal tsyn,F,D1,D2,facfactor,on_syn
         
         Vm = y[0]
         m = y[1]
@@ -71,17 +75,33 @@ def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
         n = y[3]
         
         t_last = y[7]
-        current_g_syn = y[8]
-        #print("Time: ", t, " gsyn: ",current_g_syn)
+        #current_g_syn = y[8]
+        
+        r_syn = y[10]
+        
+        tsyn = y[11]
+        F = y[12]
+        D1 = y[13]
+        D2 = y[14]
+        facfactor = y[15]
+        on_syn = y[16]
+
+        alpha_t_max_syn = .088
+        beta_syn = 0.0033
+        c_dur_syn = 10
+        current_g_syn = 0
+        
+        if tsyn>0:
+            if t-tsyn < c_dur_syn:
+                on_syn = 1
+            else:
+                on_syn = 0
 
         if True in [i >= t_last and i < t for i in syn_times]:
-            #print(current_g_syn)
-            current_g_syn = current_g_syn + syn_weight #Original
-            # short term faciliation and depression
-            
-            F  = 1 + (F-1)* math.exp(-(t - tsyn)/tauF)
-            D1 = 1 - (1-D1)*math.exp(-(t - tsyn)/tauD1)
-            D2 = 1 - (1-D2)*math.exp(-(t - tsyn)/tauD2)
+                        
+            F  = 1 + (F-1)* np.exp(-(t - tsyn)/tauF)
+            D1 = 1 - (1-D1)*np.exp(-(t - tsyn)/tauD1)
+            D2 = 1 - (1-D2)*np.exp(-(t - tsyn)/tauD2)
             
             tsyn = t
             
@@ -92,24 +112,25 @@ def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
                 F = 30     
             D1 = D1 * d1
             D2 = D2 * d2
-
+            
+            current_g_syn = gsyn + syn_weight
+            
+            if stsp:
+                current_g_syn = gsyn + syn_weight* facfactor
+            
             #print(facfactor)
-
-
-        if not stsp:
-            facfactor = 1
 
         i_Na = gNa*np.power(m,3)*h*(vNa - Vm)
         i_K =  gK*np.power(n,4)*(vK - Vm)
         i_L =  gL*(vL - Vm)
-        i_Syn = current_g_syn*facfactor*(syn_rev - Vm)
-        
+        i_Syn = current_g_syn*r_syn*(syn_rev - Vm)
+                
         dy[0] = (i_Na + i_K + i_L + i_Syn + I(t,inj_amp,inj_start,inj_dur))/C
         dy[1] = (alpha_m(Vm) * (1.0 - m)) - (beta_m(Vm) * m)
         dy[2] = (alpha_h(Vm) * (1.0 - h)) - (beta_h(Vm) * h)
         dy[3] = (alpha_n(Vm) * (1.0 - n)) - (beta_n(Vm) * n)
-        
-        
+                
+        delta_r_syn = alpha_t_max_syn*on_syn*(1-r_syn) -beta_syn*r_syn
 
         # Convienient way of saving conductances
         dy[4] = i_Na - y[4]
@@ -118,42 +139,41 @@ def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
         # Save the timestamps
         dy[7] = t - y[7]
 
-        dy[8] = -current_g_syn/syn_tau
+        dy[8] = 0#-current_g_syn/syn_tau
 
         dy[9] = i_Syn - y[9]
+        dy[10] = delta_r_syn
+        
+        dy[11] = tsyn - y[11]
+        dy[12] = F - y[12]
+        dy[13] = D1 - y[13]
+        dy[14] = D2 - y[14]
+        dy[15] = facfactor - y[15]
+        dy[16] = on_syn - y[16]
         
         return dy
 
-    inj_amp = 0 
-    #inj_start = 0
-    #inj_dur = 0
-
-    t = np.linspace(tmin, tmax, 1000)
-    Y = np.array([v_init, m_inf(), h_inf(), n_inf(), 0, 0, 0, 0, 0, 0])
-    v = odeint(hh_eq,Y,t,args=(inj_amp,inj_start,inj_dur,syn_times))
     
-    #def euler(method,Y,t_max,t_step,args=[]):
-    #    for i_t in range(0,t_max,t_step):
-    #        Y = method(Y,i_t,*args)
-    #        Y = Y + Y * t_step           
-
-    #v = euler(hh_eq,Y,tmax,.01,args=(inj_amp,inj_start,inj_dur,syn_times))
-
+    Y = np.array([v_init, m_inf(), h_inf(), n_inf(), 0, 0, 0, 0, gsyn, 0, 0,-10000,0,1,1,1,0])
+    sol = solve_ivp(hh_eq,[tmin,tmax],Y,max_step=.1)
+    v = sol.y
+    t = sol.t
+            
     f, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2, 2, figsize=(12,12))
-    ax1.plot(t,v[:,0])
+    ax1.plot(t,v[0,:])
     ax1.set(ylabel='v [mv]',xlabel='time [ms]')
     ax1.set_title('Cell Potential')
     
-    ax2.plot(t,v[:,1])
-    ax2.plot(t,v[:,2])
-    ax2.plot(t,v[:,3])
+    ax2.plot(t,v[1,:])
+    ax2.plot(t,v[2,:])
+    ax2.plot(t,v[3,:])
     ax2.set(ylabel='Probability',xlabel='time [ms]')
     ax2.legend(["m","h","n"])
     ax2.set_title('Gate Activation/Inactivation')
     
-    ax3.plot(t,v[:,4])
-    ax3.plot(t,v[:,5])
-    ax3.plot(t,v[:,6])
+    ax3.plot(t,v[4,:])
+    ax3.plot(t,v[5,:])
+    ax3.plot(t,v[6,:])
     ax3.set(ylabel='Conductance [mS/cm2]',xlabel='time [ms]')
     ax3.legend(["Na","K","Leak"])
     ax3.set_title('Channel Conductance')
@@ -167,14 +187,14 @@ def plot_hh(C = 1, vNa = 45, vK = -82, vL = -59,
     
     # limit cycles
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.plot(v[:, 0], v[:, 1], label='Vm - m')
-    ax.plot(v[:, 0], v[:, 3], label='Vm - n')
+    ax.plot(v[0,:], v[1,:], label='Vm - m')
+    ax.plot(v[0,:], v[3,:], label='Vm - n')
     ax.set_title('Limit cycles')
     ax.legend()
     plt.grid()
 
-    fig, axsyn = plt.subplots(figsize=(6, 6))
-    axsyn.plot(t, v[:, 9], label='Synapse 1')
+    fig, axsyn = plt.subplots(figsize=(20, 20))
+    axsyn.plot(t, v[9,:], label='Synapse 1')
     axsyn.set_title('Synaptic Current')
     axsyn.legend()
     plt.grid()
